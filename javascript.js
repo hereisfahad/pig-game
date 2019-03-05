e8d686da54102bdb76ff3205d026d9b0e5f1e00d
hereisfahad <fahadjabbar31@gmail.com>
2019-03-05 14:57:11 +0500

    added winning logic

--- javascript.js
+++ javascript.js
@@ -4,57 +4,83 @@ let current = 0;
 init();
 
 document.querySelector(".btn-roll").addEventListener("click",rollDice);
-document.querySelector(".btn-new").addEventListener("click",init);
+document.querySelector(".btn-new").addEventListener("click",function(){
+    location.reload();
+});
+//document.getElementById('score-0').addEventListener('change',function(){
+//   console.log('score changed'); 
+//});
+//console.log(document.getElementsByClassName('player-score'));
 
 document.querySelector(".btn-hold").addEventListener("click",holdEventHandler);
 function holdEventHandler(){
-//if hold is pressed change the active state and add current to score
-//also check value of score
-//if greater or equal to 100 return the winner state by activatin 
-//winner class and also calling the init function
+    //if hold is pressed change the active state and add current to score
+    updateScore();
+    current = 0;
+    toggleActiveClass();
+//        document.getElementById(`current-${activePlayer}`).textContent = '0';
+
+     document.getElementById("current-0").textContent = '0';
+     document.getElementById("current-1").textContent = '0';
     
 }
 
-
+function updateScore(){
+    //update score array
+//        console.log(activePlayer);
+//        console.log(current);
+//        console.log(score[activePlayer]);
+        score[activePlayer] += current;
+     //update the score of the current player
+        document.getElementById(`score-${activePlayer}`).textContent = score[activePlayer];
+     //also check value of score
+    
+    //if greater or equal to 100 return the winner state by activatin 
+    if(score[activePlayer] >=20){
+        console.log(activePlayer +"wins");
+        document.querySelector('.player-0-panel').classList.add('winner');
+        //do the winnings
+        document.querySelector(`#name-${activePlayer}`).textContent = 'Winner!';
+        //hide dice
+        document.querySelector(".dice").style.display ="none";
+    }
+    //winner class and also calling the init function
+    
+}
 function rollDice(){
     //generate random number
     let randNum = Math.floor((Math.random()*6)+1);
-    
+    document.getElementById(`current-${activePlayer}`).textContent = '0';
     //change the dice image plus unhide it
     let diceDom = document.querySelector(".dice");
     diceDom.style.display = "block";
     diceDom.src=`dice-${randNum}.png`
-    
-    //get current score of both players
-    
-    let current1 = document.getElementById("current-1");
-    
+    //if dice number is other than 1 add it to current score
     if(randNum !== 1){
         current += randNum;
-//        document.getElementById("current-0").textContent = current;
+        document.getElementById(`current-${activePlayer}`).textContent = current;
     }else{
-        document.querySelector('.player-0-panel').classList.toggle('active');
-        document.querySelector('.player-1-panel').classList.toggle('active');
-//        activePlayer = 1;
-//        console.log(activePlayer);
+        updateScore();
+        current = 0;
+        toggleActiveClass();
     }
-    current0 = document.getElementById("current-0");
-//    console.log(current0.textContent+randNum);
-
+    
 
 }
 
 function init(){
     //hide the dice
     document.querySelector(".dice").style.display ="none";
-    //get current and scores
-    let current0 = document.getElementById("current-0");
-    let current1 = document.getElementById("current-1");
-    let score0 = document.getElementById("score-0");
-    let score1 = document.getElementById("score-1");
+    //get current to zero
+     document.getElementById("current-0").textContent = '0';
+     document.getElementById("current-1").textContent = '0';
+     document.getElementById("score-0").textContent = '0'; 
+     document.getElementById("score-1").textContent = '0';
     //set current and scores to zero
-    current0.textContent = '0';
-    current1.textContent = '0';
-    score0.textContent = '0';
-    score1.textContent = '0';
+}
+function toggleActiveClass(){
+    //change the active state
+        activePlayer == 0 ?activePlayer = 1 : activePlayer = 0;
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
 }
